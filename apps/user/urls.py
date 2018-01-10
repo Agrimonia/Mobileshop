2cb709208c6f329bb0742f309bd1c79f02b42a55
Yongcong Lin <646677472@qq.com>
2018-01-10 16:30:19 +0800

    add login-beta

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import UserShow, Register
+from .views import UserShow, Login, Register
 
 urlpatterns = [
     url(r'^$', UserShow.as_view()),
+    url(r'^login/',Login.as_view()),
     url(r'^register/', Register.as_view()),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -6,20 +6,64 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
 from backend.helpers import InputErrorMessage, JSONResponse
+from user.permissions import IsOwnerOrReadOnly
 from .models import UserSerialiser, User
 
 
 # Create your views here.
 class UserShow(APIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny, )
+
+    #permission_classes = (IsOwnerOrReadOnly)
+
     def get(self, request, format=None):
         serializer = UserSerialiser(request.user)
         return JSONResponse(serializer.data)
 
-class Register(APIView):
-    permission_classes = (AllowAny, )
-    
-    def post(self, request):
 
+class Login(APIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny, )
+
+    def post(self, request):
+        try:
+            data = json.loads(request.body.decode('utf-8'))
+        except json.JSONDecodeError:
+            return InputErrorMessage("Invalid JSON body")
+        username = data.get('username')
+        password = data.get('password')
+        if "username" not in data:
+            return InputErrorMessage("username not provide.")
+        if "password" not in data:
+            return InputErrorMessage("password not provide.")
+        if User.objects.filter(username=data["username"]).exists():
+            userResult = User.objects.filter(
+                username=data["username"])
+            print(userResult)
+            return JSONResponse({
+                
+            })
+            if (len(userResult) > 0):
+                # 记忆已登录用户
+                self.request.session['user_id'] = user.id
+
+            else:
+                return JSONResponse({
+                    "code": 400,
+                    "message": "Password error.",
+                })
+        else:
+            return JSONResponse({
+                "code": 400,
+                "message": "Username error."
+            })
+
+class Register(APIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny, )
+
+    def post(self, request):
         try:
             data = json.loads(request.body.decode('utf-8'))
         except json.JSONDecodeError:
@@ -34,7 +78,10 @@ class Register(APIView):
             return InputErrorMessage("email is used.")
         if "password" not in data:
             return InputErrorMessage("password not provide.")
-        user = User.objects.create_user(username=data["username"], email=data["email"], password=data["password"])
+        user = User.objects.create_user(
+            username=data["username"],
+            email=data["email"],
+            password=data["password"])
         user.save()
         return JSONResponse({
             "code": 200,
